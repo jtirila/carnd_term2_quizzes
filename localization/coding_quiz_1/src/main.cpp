a3e35cbf169fb79c7ff7efa80a40035effd114db
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2017-11-06 12:19:53 +0200

    Finish localization quiz1

--- localization/coding_quiz_1/src/main.cpp
+++ localization/coding_quiz_1/src/main.cpp
@@ -42,14 +42,14 @@ int main() {
 	 *  read map and measurements:											     *
 	 *****************************************************************************/
 	//read map:
-	helper.read_map_data("data/map_1d.txt", map_1d);
+	helper.read_map_data("../data/map_1d.txt", map_1d);
 
 	//define file name of controls:
-	sprintf(in_file_name_ctr, "data/example%s/control_data.txt", 
+	sprintf(in_file_name_ctr, "../data/example%s/control_data.txt",
 			example_string.c_str());
 
 	//define file name of observations:
-	sprintf(in_file_name_obs, "data/example%s/observations/", 
+	sprintf(in_file_name_obs, "../data/example%s/observations/",
 			example_string.c_str());
 	
 	//read in data to measurement package list:
@@ -61,11 +61,22 @@ int main() {
 		 *  Coding quiz 1: just print out map infos and measurement package:     *
     ******************************************************************************/
 
-	/////////////
-    //Add code://	
-    /////////////
-	printf("*************************************************\n");
-	printf("Please print out map information!\n");	
-	printf("*************************************************\n");
+	for(int i = 0; i < map_1d.landmark_list.size(); i++) {
+		map::single_landmark_s landmark = map_1d.landmark_list[i];
+		cout << "id: " << landmark.id_i << ", x: " << landmark.x_f << "\n";
+	}
+
+	for(int i = 0; i < measurement_pack_list.size(); i++) {
+		MeasurementPackage meas_pack = measurement_pack_list[i];
+		cout << "control: " << meas_pack.control_s_.delta_x_f << ", observations: ";
+    for(int j = 0; j < meas_pack.observation_s_.distance_f.size(); j++) {
+			cout << meas_pack.observation_s_.distance_f[j];
+
+			// If not the last observation, output a comma and space
+			if(meas_pack.observation_s_.distance_f.size() - 1 > j)
+        cout << ", ";
+		}
+		cout << "\n";
+	}
 	return 0;
 }
