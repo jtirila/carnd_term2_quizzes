b43c1067025dc04f5edb581e4edf39c52eb5ed8d
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2017-11-06 21:02:41 +0200

    Finishing quiz 2

--- localization/coding_quiz_2/src/bayesianFilter.cpp
+++ localization/coding_quiz_2/src/bayesianFilter.cpp
@@ -21,7 +21,7 @@ bayesianFilter::bayesianFilter() {
 
     //TODO add control_std to header file
 	//set standard deviation of control to 1.0f:
-	control_std_ = 1.0;
+	control_std = 1.0;
 
 	//define size of believe, same size as map
 	bel_x.resize(100,0);
@@ -29,7 +29,7 @@ bayesianFilter::bayesianFilter() {
 	//TODO add bel_x_init to header file
 	// NOTE: helps us not overwrite believe during
 	// the motion calculation
-	//bel_x_init
+	bel_x_init.resize(100, 0);
 
 }
 
@@ -45,36 +45,42 @@ void bayesianFilter::process_measurement(const MeasurementPackage &measurements,
 	/******************************************************************************
 	 *  Set init belief of state vector:
 	 ******************************************************************************/
-  if(!is_initialized_){
+  if(!is_initialized_) {
 
     //TODO: run over map, all map_1d.lanmark_list values:
-    for (int l=0; l < map_1d.landmark_list.size(); l++){
-
-
-    //TODO: get landmark l from map
-			
+    double proba_sum = 0.0;
+    for (int l = 0; l < map_1d.landmark_list.size(); l++) {
+      map::single_landmark_s landmark = map_1d.landmark_list[l];
+      //TODO: get landmark l from map
 
       //check, if landmark position x fits in map [0,100]:
-      // if(... ){
+      if (100.0 > landmark.x_f && 0 <= landmark.x_f) {
 
-				//TODO: get landmark x position * use help_function.h for reference
-				
-				// ______/---\_____ << initial believe state at a landmark
-				
-				//TODO: set belief to 1 at position and +/- from position:
-				
+        int prev_loc = int(landmark.x_f) - 1;
 
-      } //end if
-    }//end for
+        if (0 < prev_loc) {
+          bel_x_init[prev_loc] = 1;
+        }
 
-	//TODO: normalize initial believe * use help_function.h for reference
-	//bel_x_init = 
+        bel_x_init[prev_loc + 1] = 1;
 
-	//set initial flag to true:
-	//is_initialized_ = 
-	
-	//}//end if
-	
+        if (100 > prev_loc + 2) {
+          bel_x_init[prev_loc + 2] = 1;
+        }
+      }
+    }
+
+    bel_x_init = helpers.normalize_vector(bel_x_init);
+    is_initialized_ = true;
+
+    // DEBUG:
+    // for(int i = 0; i < bel_x_init.size(); i++)
+    //   std::cout << "Straight after initialization bel[" << i << "]: " << bel_x_init[i] << "\n";
+
+  }
+
+
+
 	/******************************************************************************
 	 *  motion model and observation update
 	******************************************************************************/
@@ -85,8 +91,7 @@ void bayesianFilter::process_measurement(const MeasurementPackage &measurements,
 	MeasurementPackage::observation_s observations = measurements.observation_s_;
 
 	//run over all bel_x values (index represents the pose in x!):
-	//for (int i=0; i< ...){
-
+	for (int i=0; i< bel_x.size(); i++){
 
 		/**************************************************************************
 		 *  posterior for motion model
@@ -97,30 +102,35 @@ void bayesianFilter::process_measurement(const MeasurementPackage &measurements,
 		float posterior_motion = 0.0f;
 
 		//loop over state space x_t-1 * same size as bel_x (Perform Convolution):
-		//for (int j=0; j< ...){
-			
-			//TODO: Calculate transition probabilites using helpers.normpdf()
-			// x: difference between bel_x index and state space index
-			// mu: the movement from controls defined above
-			// std: defined eariler
-			
-			//TODO: Calculate motion model
+		for (int j=0; j < bel_x_init.size(); j++){
+
+      // TODO: Calculate transition probabilites using helpers.normpdf()
+      // x: difference between bel_x index and state space index
+      // mu: the movement from controls defined abov
+      // std: defined eariler
+      float pos_i = float(i);
+      float pos_j = float(j);
+      double trans_prob = helpers.normpdf(pos_i - pos_j, controls.delta_x_f, control_std);
+
+
+			// TODO: Calculate motion model
 			// ADD the transition prob multiplied by the intial believe 
 			// at state space index
-			//posterior_motion +=
-		//}
+			posterior_motion += bel_x_init[j] * trans_prob;
+		}
 
-		//TODO: update = motion_model 
+		//TODO: update = motion_model
 		// set as the posterior_motion
-		//bel_x[i] =
+		bel_x[i] = posterior_motion;
 
 
-	//};
-		//TODO: normalize bel_x:
-		
+	};
 
-		//TODO: set initial believe to bel_x for next time
-		//bel_x_init = 
+  //TODO: normalize bel_x:
+  bel_x = helpers.normalize_vector(bel_x);
+
+  //TODO: set initial believe to bel_x for next time
+  bel_x_init = bel_x;
 	 
 	 
 	 
